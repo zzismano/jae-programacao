d38b338f8c853f2d9f31b60aa18aa998f053d466
Pedro Bolliger <pedrobolliger@gmail.com>
2023-10-26 14:26:30 -0300

    mais ajustes no expansor

--- javascript/expansor.js
+++ javascript/expansor.js
@@ -4,12 +4,14 @@ $(document).ready(function() {
   var moretext = "Ver Mais";
   var lesstext = "Ver Menos";
 
+
   $('.sinopse p').each(function() {
       var content = $(this).html();
+      var $legenda = $(this).siblings(":last")
       if (content.length > showChar) {
           var c = content.substr(0, showChar);
           var h = content.substr(showChar, content.length - showChar);
-          var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent" style="display: none;"><span>' + h + '</span></span>';
+          var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent" style="display: none;"><span>' + h + '<br></br>' + $legenda.html() + '</span></span>';
           var button = '<br><a href="#" class="morelink">' + moretext + '</a>';
           $(this).html(html + button);
       }
@@ -17,6 +19,7 @@ $(document).ready(function() {
 
   $(".morelink").click(function() {
       var $content = $(this).prev().prev();
+      var $ellipses = $('.moreellipses')
       if ($(this).hasClass("less")) {
           $(this).removeClass("less");
           $(this).html(moretext);
@@ -25,6 +28,8 @@ $(document).ready(function() {
           $(this).html(lesstext);
       }
       $content.toggle();
+      $ellipses.toggle();
+
       return false;
   });
 });
