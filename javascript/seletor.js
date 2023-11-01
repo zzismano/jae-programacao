d982b1f4163eedd11753d01baa1780ee96571618
Pedro Bolliger <pedrobolliger@gmail.com>
2023-11-01 15:14:22 -0300

    mudanças no seletor, menu das categoria

--- javascript/seletor.js
+++ javascript/seletor.js
@@ -3,12 +3,10 @@ const musica = document.getElementById('musica')
 const teatro = document.getElementById('teatro')
 const evento = document.getElementById('evento')
 const expo = document.getElementById('expo')
-const ideias = document.getElementById('ideias')
-const livros = document.getElementById('livros')
 const mais = document.getElementById('mais')
 const logo = document.querySelector('.navbar-brand')
 const conteudoHome = document.getElementById('conteudo-home')
-const categorias = [filme, musica, teatro, mais, evento, expo, ideias, livros]
+const categorias = [filme, musica, teatro, mais, evento, expo]
 
 logo.addEventListener("click", (event)=> {
 
@@ -17,6 +15,13 @@ logo.addEventListener("click", (event)=> {
     var bgColor = categoria.style.backgroundColor
     var color = categoria.style.color
     var conteudo = document.getElementById(`conteudo-${categoria.id}`)
+
+    categoria.classList.remove("transition")
+    if (!categoria.lastElementChild.classList.contains("d-none")) {
+      categoria.lastElementChild.classList.add("d-none")
+    }
+
+
     if (!conteudo.classList.contains('d-none')) {
       conteudo.classList.add('d-none')
     };
@@ -25,6 +30,7 @@ logo.addEventListener("click", (event)=> {
       categoria.style.backgroundColor = color;
       categoria.style.color = "white"
     }
+
 
 
   })
@@ -36,29 +42,35 @@ categorias.forEach((categoria) => {
     var color = event.currentTarget.style.color
     var conteudo = document.getElementById(`conteudo-${categoria.id}`)
 
+    conteudo.classList.remove('d-none')
+    event.currentTarget.classList.toggle("transition")
+
+
+    event.currentTarget.lastElementChild.classList.toggle("d-none")
+
     if (!conteudoHome.classList.contains('d-none')) {
       conteudoHome.classList.add('d-none')
+
     };
 
-    if (event.currentTarget.style.color === 'white' ) {
-      event.currentTarget.style.backgroundColor = 'white';
-      event.currentTarget.style.color = bgColor;
-    };
+    // if (event.currentTarget.style.color === 'white' ) {
+    //   event.currentTarget.style.backgroundColor = 'white';
+    //   event.currentTarget.style.color = bgColor;
+    // };
 
     var copia = categorias.slice()
     copia.splice(categorias.indexOf(event.currentTarget), 1)
 
     copia.forEach((elemento) => {
-      if (elemento.style.backgroundColor === "white") {
-        elemento.style.backgroundColor = elemento.style.color;
-        elemento.style.color = "white"
+      if (elemento.classList.contains("transition")) {
+        elemento.classList.remove("transition")
+        elemento.lastElementChild.classList.add("d-none")
       };
 
 
     });
 
 
-    conteudo.classList.remove('d-none')
 
     copia.forEach((elemento) => {
       var conteudoCopia = document.getElementById(`conteudo-${elemento.id}`)
